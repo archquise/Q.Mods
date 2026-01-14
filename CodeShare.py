95c308b10af0d83ece1e72f6677e143893547f79
Archquise <archquise@gmail.com>
2026-01-15 02:24:45 +0400

    refactor: CodeShare

--- CodeShare.py
+++ CodeShare.py
@@ -17,7 +17,8 @@
 # ---------------------------------------------------------------------------------
 
 import logging
-import os
+from http import HTTPStatus
+from pathlib import Path
 
 import aiofiles
 import aiohttp
@@ -30,58 +31,58 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class CodeShareMod(loader.Module):
-    """Uploads your code at the kmi.aeza.net (Pastebin and GitHub Gist alternative)"""
+    """Uploads your code at the kmi.aeza.net (Pastebin and GitHub Gist alternative)."""
 
-    strings = {
+    strings = { # noqa: RUF012
         "name": "CodeShare",
         "invalid_args": "<emoji document_id=5854929766146118183>❌</emoji> There is no arguments or reply with a file, or they are invalid",
         "_cls_doc": "Uploads your code at the kmi.aeza.net (Pastebin and GitHub Gist alternative)",
         "link_ready": "<emoji document_id=5854762571659218443>✅</emoji> <b>Code uploaded! Link:</b> <code>{}</code>",
     }
 
-    strings_ru = {
+    strings_ru = { # noqa: RUF012
         "_cls_doc": "Загружает ваш код на kmi.aeza.net (альтернатива Pastebin и GitHub Gist)",
         "invalid_args": "<emoji document_id=5854929766146118183>❌</emoji> Нет аргументов или реплая с файлом, или они неверны",
         "link_ready": "<emoji document_id=5854762571659218443>✅</emoji> <b>Код загружен! Ссылка:</b> <code>{}</code>",
     }
 
     async def upload_to_kmi(self, content: str) -> str:
+        """Upload text to kmi.aeza.net."""
         url = "https://kmi.aeza.net"
         data = aiohttp.FormData()
         data.add_field("kmi", content)
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, data=data) as response:
-                if response.status == 200:
-                    link = await response.text()
-                    return link
-                logger.error(f"Error occurred! Status code: {response.status}")
+        async with aiohttp.ClientSession() as session, \
+            session.post(url, data=data) as response:
+                if response.status == HTTPStatus.OK:
+                    return await response.text()
+                logger.error("Error occurred! Status code: %s", response.status)
                 return None
 
     @loader.command(
         ru_doc="Загрузка кода на сайт",
         en_doc="Upload code to the site",
     )
-    async def codesharecmd(self, message):
+    async def codesharecmd(self, message) -> None: # noqa: ANN001, D102
         args = utils.get_args(message)
         reply = await message.get_reply_message()
         if args:
-             async with aiohttp.ClientSession() as session:
-                async with session.get(args[0]) as response:
-                    if response.status == 200:
+             async with aiohttp.ClientSession() as session, \
+                session.get(args[0]) as response:
+                    if response.status == HTTPStatus.OK:
                         content = await response.text()
                         link = await self.upload_to_kmi(content)
                         await utils.answer(message, self.strings["link_ready"].format(link))
                         return
-                    logger.error(f"Error occurred! Status code: {response.status}")
-                    return None
+                    logger.error("Error occurred! Status code: %s", response.status)
+                    return
         if reply and isinstance(reply.media, MessageMediaDocument):
             file_name = await reply.download_media()
             async with aiofiles.open(file_name) as f:
                 content = await f.read()
             try:
-                os.remove(file_name)
-            except TypeError as e:
+                Path(file_name).unlink()
+            except Exception as e: # noqa: BLE001
                 logger.warning(e)
             link = await self.upload_to_kmi(content)
             await utils.answer(message, self.strings["link_ready"].format(link))
